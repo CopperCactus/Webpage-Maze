e1e04ae711c0a2c3d4368770311666605b5940e2
CopperCactus <cooperkuglin@gmail.com>
2024-05-26 22:35:13 -0400

    Beginnings of transfer

---
+++ scripts/baseball.js
@@ -0,0 +1,3 @@
+setTimeout(() => {
+    window.location.href = "broken.html";
+}, 1250);
---
+++ scripts/pills.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const redButton = document.querySelector(".red-button");
+    redButton.addEventListener("click", function() {
+      window.open("https://youtu.be/zE7PKRjrid4?t=115", "_blank");
+    });
+});
