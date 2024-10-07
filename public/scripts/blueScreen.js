9e8545250d661e4c6996f3cd7a3e89f515534025
rhit-kuglinwc <kuglinwc@rose-hulman.edu>
2024-10-07 12:18:40 -0400

    Revert "syncing"
    This reverts commit 8a270d76eecaf7732ead8d2b16aad5786c1fc1ea.

--- public/scripts/blueScreen.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("keydown", function(event) {
-    console.log(event);
-    if (event.keyCode === 13) { 
-        window.location.href = "window.html";
-    }
-    else{
-        window.location.href = "continue.html";
-    }
-  });
