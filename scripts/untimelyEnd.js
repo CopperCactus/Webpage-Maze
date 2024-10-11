6f4b2dd2efd17c833b129538de28ddc64bffc3d1
rhit-kuglinwc <kuglinwc@rose-hulman.edu>
2024-10-10 23:50:18 -0400

    play fix

--- scripts/untimelyEnd.js
+++ scripts/untimelyEnd.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
     let timer = 0;
-    var carSound = document.getElementById("car-pulling-up-sound");
-    var defibSound = document.getElementById("defibrillator-sound");
     setTimeout(function() {
         //Play car pull-up sound
+        var carSound = document.getElementById("car-pulling-up-sound");
         carSound.play();
         setTimeout(() => {
             //Play defibrillator sound
+            var defibSound = document.getElementById("defibrillator-sound");
             defibSound.play();
             setTimeout(function() {
                 //TODO: Revival
