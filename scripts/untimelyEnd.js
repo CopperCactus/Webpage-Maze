b97ce13c59d17806a7b77cebc6ce8e97d90f9825
rhit-kuglinwc <kuglinwc@rose-hulman.edu>
2024-10-10 23:46:42 -0400

    untimely end update

--- scripts/untimelyEnd.js
+++ scripts/untimelyEnd.js
@@ -1,17 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
     let timer = 0;
+    var carSound = document.getElementById("car-pulling-up-sound");
+    var defibSound = document.getElementById("defibrillator-sound");
     setTimeout(function() {
         //Play car pull-up sound
-        var carSound = document.getElementById("car-pulling-up-sound");
         carSound.play();
         setTimeout(() => {
             //Play defibrillator sound
-            var defibSound = document.getElementById("defibrillator-sound");
             defibSound.play();
-        setTimeout(function() {
-            //TODO: Revival
-            window.location.href = "revivalChoice.html";
-        }, 15000)
+            setTimeout(function() {
+                //TODO: Revival
+                window.location.href = "revivalChoice.html";
+            }, 16000);
         }, 44000);
     }, 5000)
 });
