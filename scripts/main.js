25ca173d1028a775657f404a75e65227cd7ba3e7
rhit-kuglinwc <kuglinwc@rose-hulman.edu>
2024-10-24 16:17:05 -0400

    idk man i forgor

--- scripts/main.js
+++ scripts/main.js
@@ -7,6 +7,9 @@
  */
 
 
+// import * as THREE from 'three';
+
+
 /** namespace. */
 var rhit = rhit || {};
 
@@ -474,13 +477,30 @@ rhit.initializePage = function () {
 /** function and class syntax examples */
 rhit.main = function () {
 
+	// const scene = new THREE.Scene();
+	// const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+	// const renderer = new THREE.WebGLRenderer();
+	// renderer.setSize(window.innerWidth, window.innerHeight);
+	// renderer.setAnimationLoop(animate);
+	// document.body.appendChild(renderer.domElement);
+	// const geometry = new THREE.BoxGeometry(1, 1, 1);
+	// const material = new THREE.MeshBasicMaterial({
+	// 	color: 0x00ff00
+	// });
+	// const cube = new THREE.Mesh(geometry, material);
+	// scene.add(cube);
+	// camera.position.z = 5;
+
+	// function animate() {
+	// 	cube.rotation.x += 0.01;
+	// 	cube.rotation.y += 0.01;
+	// 	renderer.render(scene, camera);
+	// }
+
 	console.log("Ready");
 
 	var w = window.innerWidth;
 	var h = window.innerHeight;
-	if (document.querySelector("#img-well")) {
-
-	}
 
 	rhit.fbAuthManager = new rhit.FbAuthManager();
 	rhit.fbAuthManager.beginListening(() => {
